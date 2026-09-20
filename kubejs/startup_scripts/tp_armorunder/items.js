4d5cfa6e3c76bb3b90356bc8ba75ffbf688b7b2b
立秋枫林晚 <animator70@outlook.com>
2026-09-20 14:13:43 +0800

    暮色森林压根没用饮品（百科参考没用）；新增内衬本地化

--- kubejs/startup_scripts/tp_armorunder/items.js
+++ kubejs/startup_scripts/tp_armorunder/items.js
@@ -1,32 +1,76 @@
-StartupEvents.registry("item", event => {
+StartupEvents.registry("item", event =>
+{
+    const _this = id => "tp_armorunder:" + id;
+
     // 奥兹内衬
-    event.create("tp_armorunder:auto_helmet_liner", "basic");
-    event.create("tp_armorunder:auto_chestplate_liner", "basic");
-    event.create("tp_armorunder:auto_leggings_liner", "basic");
-    event.create("tp_armorunder:auto_boots_liner", "basic");
+    event.create(_this("auto_helmet_liner"), "basic");
+    event.create(_this("auto_chestplate_liner"), "basic");
+    event.create(_this("auto_leggings_liner"), "basic");
+    event.create(_this("auto_boots_liner"), "basic");
 
     // 保温内衬
-    event.create("tp_armorunder:beekeeper_helmet_liner", "basic");
-    event.create("tp_armorunder:beekeeper_chestplate_liner", "basic");
-    event.create("tp_armorunder:beekeeper_leggings_liner", "basic");
-    event.create("tp_armorunder:beekeeper_boots_liner", "basic");
+    event.create(_this("beekeeper_helmet_liner"), "basic");
+    event.create(_this("beekeeper_chestplate_liner"), "basic");
+    event.create(_this("beekeeper_leggings_liner"), "basic");
+    event.create(_this("beekeeper_boots_liner"), "basic");
 
     // 降温内衬
-    event.create("tp_armorunder:cool_helmet_liner", "basic");
-    event.create("tp_armorunder:cool_chestplate_liner", "basic");
-    event.create("tp_armorunder:cool_leggings_liner", "basic");
-    event.create("tp_armorunder:cool_boots_liner", "basic");
+    event.create(_this("cool_helmet_liner"), "basic");
+    event.create(_this("cool_chestplate_liner"), "basic");
+    event.create(_this("cool_leggings_liner"), "basic");
+    event.create(_this("cool_boots_liner"), "basic");
 
     // 加热凝胶
-    event.create("tp_armorunder:heating_goo", "basic");
+    event.create(_this("heating_goo"), "basic");
     // 加热丝线
-    event.create("tp_armorunder:flyweight_thread", "basic");
+    event.create(_this("flyweight_thread"), "basic");
 
     // 降温凝胶
-    event.create("tp_armorunder:cooling_goo", "basic");
+    event.create(_this("cooling_goo"), "basic");
     // 降温丝线
-    event.create("tp_armorunder:cool_liner_material", "basic");
+    event.create(_this("cool_liner_material"), "basic");
 
     // 奥兹丝线
-    event.create("tp_armorunder:flipflop_liner_material", "basic");
+    event.create(_this("flipflop_liner_material"), "basic");
+});
+
+StartupEvents.registry("creative_mode_tab", event =>
+{
+    const _this = id => "tp_armorunder:" + id;
+
+    // 注册创造物品栏，并给予创造物品栏id
+    const tab = event.create(_this("items"));
+
+    // 设置创造物品栏的图标,注意这里的物品一定要是存在的
+    tab.icon(() => Item.of(_this("auto_chestplate_liner")));
+    // 设置创造物品栏的显示名称
+    tab.displayName = Text.translatable("item_group.tp_armorunder.items");
+    //往物品栏里添加物品
+    tab.content(() => [
+        // 奥兹内衬
+        _this("auto_helmet_liner"),
+        _this("auto_chestplate_liner"),
+        _this("auto_leggings_liner"),
+        _this("auto_boots_liner"),
+        // 保温内衬
+        _this("beekeeper_helmet_liner"),
+        _this("beekeeper_chestplate_liner"),
+        _this("beekeeper_leggings_liner"),
+        _this("beekeeper_boots_liner"),
+        // 降温内衬
+        _this("cool_helmet_liner"),
+        _this("cool_chestplate_liner"),
+        _this("cool_leggings_liner"),
+        _this("cool_boots_liner"),
+        // 加热凝胶
+        _this("heating_goo"),
+        // 加热丝线
+        _this("flyweight_thread"),
+        // 降温凝胶
+        _this("cooling_goo"),
+        // 降温丝线
+        _this("cool_liner_material"),
+        // 奥兹丝线
+        _this("flipflop_liner_material")
+    ]);
 });
