7e8d88f29335e16cb3460b715ea5694e3971040d
立秋枫林晚 <2226531799@qq.com>
2026-09-22 12:04:12 +0800

    禁用灾变一些超模武器

--- kubejs/client_scripts/cataclysm/ban.js
+++ kubejs/client_scripts/cataclysm/ban.js
@@ -5,6 +5,21 @@ let cataclysm_ban_items = [
 	"cataclysm:black_steel_shovel",
 	"cataclysm:black_steel_hoe",
 	"cataclysm:black_steel_targe",
+	"cataclysm:the_immolator",
+	"cataclysm:the_annihilator",
+	"cataclysm:soul_render",
+	"cataclysm:wrath_of_the_desert",
+	"cataclysm:cursed_bow",
+	"cataclysm:the_incinerator",
+	"cataclysm:gauntlet_of_maelstrom",
+	"cataclysm:gauntlet_of_bulwark",
+	"cataclysm:bulwark_of_the_flame",
+	"cataclysm:meat_shredder",
+	"cataclysm:laser_gatling",
+	"cataclysm:wither_assault_shoulder_weapon",
+	"cataclysm:void_assault_shoulder_weapon",
+	"cataclysm:infernal_forge",
+	"cataclysm:void_forge",
 ];
 let cataclysm_ban_wearables = [
 ];
